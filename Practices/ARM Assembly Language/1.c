95eed5e493de5a429f2704061b731a1c65a489b4
Mark Chen <mark.chen.sstm@gmail.com>
2021-10-13 09:17:21 +0800

    update (1_v2)

---
+++ Practices/ARM Assembly Language/1.c
@@ -0,0 +1,21 @@
+#include<stdio.h>   
+
+unsigned int Count1Bits(unsigned int num);
+
+int main(void){
+    unsigned int Value=0x5AA555AA;
+    unsigned int Count;
+
+    Count=Count1Bits(Value);
+
+    printf("Value=%08X, Count=%d\n", Value, Count);
+    return 0;
+}
+
+unsigned int Count1Bits(unsigned int num){
+   __asm__(
+       ".Start_"
+       "MOV r1, #0 @Move r1 with number 0"
+       ""
+   );
+}
